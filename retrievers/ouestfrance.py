cb81989ca93b98ef7a23d4b7e1a414deb84712f6
syffer <maxime.nicolas2@hotmail.fr>
2020-08-24 16:24:13 +0200

    fix: ouestfrance retrieved doesn't get locations anymore

--- retrievers/ouestfrance.py
+++ retrievers/ouestfrance.py
@@ -36,7 +36,7 @@ class OuestFranceLocationRetriever(retrievers.retriever.LocationRetriever):
         result = requests.get(url, params=params)
 
         soup = bs4.BeautifulSoup(result.text, features="html.parser")
-        tags = soup.find_all("a > div:not[.premium]", class_="annLink")
+        tags = soup.find_all("a", class_="annLink")
         tags = [tag for tag in tags if "premium" not in tag["class"]]   # removes premium as they don't have dates
 
         parsed_url = urllib.parse.urlparse(url)
